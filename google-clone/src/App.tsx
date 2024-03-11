d612d72e3f023843f40fae762dc056e842986824
adrianacostep33 <adriana.coste@principal33.com>
2024-03-11 20:15:28 +0200

    Set up project

---
+++ google-clone/src/App.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import SearchPage from "./pages/SearchPage";
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<SearchPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
