3f59cd2b98526bea49aed4748667b1fe8ef0f00f
adrianacostep33 <adriana.coste@principal33.com>
2024-03-12 17:16:39 +0200

    Add dummy firestore query

---
+++ google-clone/src/firebase.tsx
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBRhYmy0p44rnVygavhEmDexCvA3kfWZnY",
+  authDomain: "clone-aea5c.firebaseapp.com",
+  projectId: "clone-aea5c",
+  storageBucket: "clone-aea5c.appspot.com",
+  messagingSenderId: "710446065353",
+  appId: "1:710446065353:web:c3373411abc25553140f18",
+  measurementId: "G-SKNSLTWN48",
+};
+
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
